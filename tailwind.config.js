05c7197d3165b6e8e39cb33cfd49314301e71879
JoelVStan <joelvstan@gmail.com>
2023-09-02 11:30:05 +0530

    Website ready - Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"]
+    },
+    screens: {
+      'mobile': '407px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
